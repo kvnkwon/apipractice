321edba75f426fcc0aef851275ed87a4c3ed8c7d
Kevin <kvnkwon@gmail.com>
2015-07-29 19:35:13 -0400

    Display movie titles with search

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 $(document).ready(function(){
 
-  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-  //Receive data from the server. The request is passed to the function as a parameter.
-    console.log(data);
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
   });
+
+  function getRequest(searchTerm){
+    var url = 'http://www.omdbapi.com';
+    var params = {
+      s: searchTerm,
+      r: 'json'
+    };
+
+    $.getJSON(url, params, function(data){
+    //Receive data from the server. The request is passed to the function as a parameter.
+      showResults(data.Search); //data.Search isolates just the Search array.
+    });
+  }
+
+  function showResults(results){
+    var html = "";
+    for(var i=0,j=results.length;i<j;i++) {
+      html += '<p>' + results[i].Title + '</p>';
+    }
+    $('#search-results').html(html);
+  }
 
 });
